f9f86a7a4801d0a007aede4cee84b9fb3072614d
Sanjeev Thapa <sanjeevthapa2924@gmail.com>
2023-07-15 06:56:29 +0545

    Update main-test.py

--- main-test.py
+++ main-test.py
@@ -2,6 +2,7 @@
 from main import Add
 
 def TestAdd():
+        print("Unit test file .......")
         assert Add(2,3) == 5
         assert Add(5,5) == 10
         print("Add Function works correctly")
