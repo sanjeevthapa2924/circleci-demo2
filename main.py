2baec88b8995d4fac2fdb225b188b8ac468088bd
Sanjeev Thapa <sanjeevthapa2924@gmail.com>
2023-07-15 06:39:21 +0545

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("Hello sanjeev , checking crcle CI ... for CICD")
+        print("Hello sanjeev , checking crcle CI ... and deployement inside container")
 
 if __name__ == '__main__':
         SayHello()
